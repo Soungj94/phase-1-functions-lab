84cc25ea14d0ba10bde5530916a2c8f3d7439678
Soungjae <soungj94@gmail.com>
2023-03-20 20:26:40 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blocks) {
+  //returns the number of blocks given a value
+  return Math.abs(blocks - 42);
+}
+function distanceFromHqInFeet(blocks) {
+  const feetPerBlock = 264;
+  return distanceFromHqInBlocks(blocks) * feetPerBlock;
+  // call the distanceFromHqInBlocks function from inside the distanceFromHqInFeet function,
+  // passing the argument from distanceFromHqInFeet into distanceFromHqInBlocks
+  // the return value of distanceFromHqInBlocks can then be used to calculate feet
+}
+
+function distanceTravelledInFeet(start, destination) {
+  //returns the number of feet traveled
+  const feetPerBlocks = 264;
+  return Math.abs(start - destination) * feetPerBlocks;
+}
+
+function calculatesFarePrice(start, destination) {
+  //returns the fare for the customer
+  const distance = distanceTravelledInFeet(start, destination);
+
+  if (distance <= 400) {
+    return 0;
+  } else if (distance > 400 && distance <= 2000) {
+    return (distance - 400) * 0.02;
+  } else if (distance > 2000 && distance <= 2500) {
+    return 25;
+  } else {
+    return "cannot travel that far";
+  }
+}
